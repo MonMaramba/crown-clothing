453d8e25e7dc8c71e6531e6c7e4fd0148b7a509d
unknown <monmaramba@yahoo.com>
2019-11-28 08:49:30 -0800

    react router dom setup and routes start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS Page</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        {/* when Switch finds exact route match, it will only render that
+        component */}
+        <Route
+          exact
+          /*boolean value, true by default */
+
+          path="/"
+          /* string value, / is the base */
+
+          component={Homepage}
+          //main arguments exact path component(component to be rendered)
+        />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
+      {/* <Homepage /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // Component that will wrap around our App
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    {/* BrowserRouter component that will provide all routing functionalities of
+    that library */}
+    {/*  */}
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
