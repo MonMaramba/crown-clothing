76afa925a407c6df21a86c8e1dac0bc788c394d5
unknown <monmaramba@yahoo.com>
2019-11-28 12:28:22 -0800

    withRouter props from react-router-dom in menu-item functional

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,8 +1,14 @@
 import React from "react";
-import "./menu-item.scss";
+import { withRouter } from "react-router-dom"; // rather than prop tunneling(or drilling), withRouter is a higher order component that returns a modified component that will have access to router props
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+import "./menu-item.scss";
+import { link } from "fs";
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{ backgroundImage: ` url(${imageUrl})` }}
@@ -14,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); // pass component into withRouter to return supercharged component with access to location, match, history.
