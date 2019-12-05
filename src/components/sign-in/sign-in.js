32ea27844ea206dd85856f463212d9da7bbd060c
unknown <monmaramba@yahoo.com>
2019-12-04 19:49:01 -0800

    firebase setup

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
+import { signInWithGoogle } from "../../firebase/firebase";
 
 import "./sign-in.scss";
 
@@ -48,6 +49,9 @@ class SignIn extends React.Component {
           />
 
           <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Sign In With Google
+          </CustomButton>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAA93_rixdL3reLl5NTRnD8Pyf2yseEv0Q",
+  authDomain: "crowndb-c5b35.firebaseapp.com",
+  databaseURL: "https://crowndb-c5b35.firebaseio.com",
+  projectId: "crowndb-c5b35",
+  storageBucket: "crowndb-c5b35.appspot.com",
+  messagingSenderId: "530255691782",
+  appId: "1:530255691782:web:848d781a24d93ca1848387",
+  measurementId: "G-H918PPP17S"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
