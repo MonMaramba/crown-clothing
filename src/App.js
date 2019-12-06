d97afcf5e72d21d230d1797a062b03f0743b28a5
unknown <monmaramba@yahoo.com>
2019-12-05 18:46:16 -0800

    firebase auth setup

--- src/App.js
+++ src/App.js
@@ -7,36 +7,62 @@ import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page";
 
+import { auth } from "./firebase/firebase";
+
 // const HatsPage = () => (
 //   <div>
 //     <h1>HATS Page</h1>
 //   </div>
 // );
-function App() {
-  return (
-    <div>
-      <Header />
-      {/*Header component placed outside of <Switch>component will always be rendered whichever route is rendered */}
-      <Switch>
-        {/* when Switch finds exact route match, it will only render that
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  // property on class
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    // communication between app and firebase is always open as long as component is in the DOM
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      // will give a function that will close the communication when called
+      this.setState({ current: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // to close the subscription when component is unmounted
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {/*Header component placed outside of <Switch>component will always be rendered whichever route is rendered */}
+        <Switch>
+          {/* when Switch finds exact route match, it will only render that
         component */}
-        <Route
-          exact
-          /*boolean value, true by default */
+          <Route
+            exact
+            /*boolean value, true by default */
 
-          path="/"
-          /* string value, / is the base */
+            path="/"
+            /* string value, / is the base */
 
-          component={Homepage}
-          //main arguments exact path component(component to be rendered)
-        />
-        {/* <Route exact path="/hats" component={HatsPage} /> */}
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUp} />
-      </Switch>
-      {/* <Homepage /> */}
-    </div>
-  );
+            component={Homepage}
+            //main arguments exact path component(component to be rendered)
+          />
+          {/* <Route exact path="/hats" component={HatsPage} /> */}
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUp} />
+        </Switch>
+        {/* <Homepage /> */}
+      </div>
+    );
+  }
 }
-
 export default App;
