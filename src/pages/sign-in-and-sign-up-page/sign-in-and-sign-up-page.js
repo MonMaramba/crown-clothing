b61e9db041d98acf6731eab47a99e1b2541192ef
unknown <monmaramba@yahoo.com>
2019-12-14 17:01:41 -0800

    sign up complete

--- src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.js
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.js
@@ -2,10 +2,12 @@ import React from "react";
 import SignIn from "../../components/sign-in/sign-in";
 
 import "./sign-in-and-sign-up-page.scss";
+import SignUp from "../../components/sign-up/sign-up";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
