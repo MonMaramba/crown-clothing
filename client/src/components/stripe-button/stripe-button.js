dcb58268582567d74a84f6b63a3c88b6168098f2
unknown <monmaramba@yahoo.com>
2020-01-29 19:14:48 -0800

    backend stripe for payments. Separated frontend into the client folder

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -1,12 +1,30 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_uVLkpg9RT3M61GFPT6ONju83003Rjwfkvj";
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    // axios is a function that receives an object with the properties that we need to pass
+    // it then gives back a promise
+    axios({
+      url: "payment", // request to the payment route. Appends "payment" to current route
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card"
+        );
+      });
   };
 
   return (
