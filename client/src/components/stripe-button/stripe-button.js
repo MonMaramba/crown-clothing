f40bc9cd260c1415519f03fed19a8669f5453cfd
unknown <monmaramba@yahoo.com>
2020-01-29 20:34:49 -0800

    stripe button test fix

--- client/src/components/stripe-button/stripe-button.js
+++ client/src/components/stripe-button/stripe-button.js
@@ -1,3 +1,49 @@
+// import React from "react";
+// import StripeCheckout from "react-stripe-checkout";
+// import axios from "axios";
+
+// const StripeCheckoutButton = ({ price }) => {
+//   const priceForStripe = price * 100;
+//   const publishableKey = "pk_test_uVLkpg9RT3M61GFPT6ONju83003Rjwfkvj";
+//   const onToken = token => {
+//     // axios is a function that receives an object with the properties that we need to pass
+//     // it then gives back a promise
+//     axios({
+//       url: "payment", // request to the payment route. Appends "payment" to current route
+//       method: "post",
+//       data: {
+//         amount: priceForStripe,
+//         token
+//       }
+//     })
+//       .then(response => {
+//         alert("Payment successful");
+//       })
+//       .catch(error => {
+//         console.log("Payment error: ", error);
+//         alert(
+//           "There was an issue with your payment. Please make sure you use the provided credit card"
+//         );
+//       });
+//   };
+
+//   return (
+//     <StripeCheckout
+//       label="Pay Now"
+//       name="CROWN Clothing Ltd."
+//       billingAddress
+//       shippingAddress
+//       image="https://svgshare.com/i/CUz.svg"
+//       description={`Your total is $${price}`}
+//       amount={priceForStripe}
+//       panelLabel="Pay Now"
+//       token={onToken}
+//       stripeKey={publishableKey}
+//     />
+//   );
+// };
+
+// export default StripeCheckoutButton;
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
@@ -5,24 +51,23 @@ import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_uVLkpg9RT3M61GFPT6ONju83003Rjwfkvj";
+
   const onToken = token => {
-    // axios is a function that receives an object with the properties that we need to pass
-    // it then gives back a promise
     axios({
-      url: "payment", // request to the payment route. Appends "payment" to current route
+      url: "payment",
       method: "post",
       data: {
         amount: priceForStripe,
-        token
+        token: token
       }
     })
       .then(response => {
-        alert("Payment successful");
+        alert("succesful payment");
       })
       .catch(error => {
-        console.log("Payment error: ", error);
+        console.log("Payment Error: ", error);
         alert(
-          "There was an issue with your payment. Please make sure you use the provided credit card"
+          "There was an issue with your payment! Please make sure you use the provided credit card."
         );
       });
   };
@@ -30,7 +75,7 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label="Pay Now"
-      name="CROWN Clothing Ltd."
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
