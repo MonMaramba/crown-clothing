4e5f13da31c73b1fd3176d2e5e4fb156beeb8ed2
unknown <monmaramba@yahoo.com>
2020-03-15 10:56:53 -0700

    root-saga created and changed from takeEvery to takeLatest saga

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 // will allow the browser to cache the store depending on config
 import { persistStore } from "redux-persist";
-import thunk from "redux-thunk";
+
 import createSagaMiddleware from "redux-saga";
 
-import { fetchCollectionsStart } from "./shop/shop.sagas";
+import rootSaga from "./root-saga";
 
 import rootReducer from "./root-reducer";
 
@@ -15,7 +15,7 @@ const middlewares = [sagaMiddleware, logger];
 // technically, variable is not exporting from the next 2 lines
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 // persisted version of the store
 export const persistor = persistStore(store);
